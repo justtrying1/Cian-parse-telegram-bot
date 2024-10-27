4e0b999aae1ededace3ff15e040e5606bc2008e3
justtrying1 <potapoffilya@gmail.com>
2024-10-28 02:44:49 +0300

    lol

---
+++ abbbbbb.py
@@ -0,0 +1,277 @@
+import cloudscraper
+import bs4
+from cianparser.constants import STREET_TYPES, NOT_STREET_ADDRESS_ELEMENTS, FLOATS_NUMBERS_REG_EXPRESSION
+from cianparser.helpers import define_price_data
+from cianparser.helpers import define_location_data
+from cianparser.helpers import define_rooms_count
+from cianparser.helpers import define_author
+from cianparser.helpers import define_specification_data
+import pickle
+from cianparser.helpers import union_dicts
+from functools import partial
+import time
+import json
+from datetime import datetime
+import os
+import sys
+import cianparser
+from cianparser.constants import CITIES
+import re
+import copy
+
+class Sent(Exception): pass
+def offer_list_updated(offer_list ,disappeared, appeared):
+   
+    for d in disappeared:
+        print(d)
+    for i in range(len(disappeared)):
+        for j in range(len(offer_list)):
+            if offer_list[j]['url'] == disappeared[i]['url']:
+                offer_list.pop(j)
+                print(len(offer_list))
+                break
+   
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    return offer_list + appeared
+
+
+def get_image_data(block):
+    img_list = {}
+    d= 0
+    for i in block.select_one("div[data-name=Gallery]").findAll("img"):
+        img_list["image{}".format(d)] = i['src']
+        d+=1
+    if img_list == {}:
+        return {"image0":"нет картинки"}
+    return img_list
+
+
+
+def city_dict():
+    city_dict = dict()
+    for i in CITIES:
+        city_dict[i[0]] = i[1]
+    print(city_dict["Москва"])
+    return city_dict
+city_dict = city_dict()
+
+
+
+def return_non_repetitive_records(file1, file2): #old, new
+    
+    result = copy.deepcopy(file1) #old_json[i]
+    result_ = copy.deepcopy(file2) #offer_list_global[i]
+    d=0
+    break_flag = False
+    while True:
+        if(break_flag):
+            break
+        flag = False  
+        
+        for i in range(len(result_)):
+            if(len(result_)-1 == i):  
+                break_flag = True
+            for j in range(len(result)):        
+                if result[j]['url'] == result_[i]['url'] :
+                    
+                    result.pop(j)
+                    result_.pop(i)
+                    flag = True     
+                    break
+            if(flag):
+                break
+    
+    return result, result_ #disappeared, appeared
+
+
+OLD_JSON = "&rent&30000&40000&1 room&Москва&2024-09-30 22-40-46.json"
+def load_old():
+    while True:
+        try:
+
+            with open(OLD_JSON, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except:
+            pass
+def get_urls(i, min_price, max_price, city, deal_type,room2=0, room1=1, page=1, room3=0, no_room = False):
+    url_list=[]
+    time.sleep(3.5)
+    offer_list = []
+    session = cloudscraper.create_scraper()
+    if no_room:
+        url_base = partial("https://www.cian.ru/cat.php?currency=2&p={page}&deal_type={deal_type}&engine_version=2&sort=creation_date_desc&maxprice={max_price}&minprice={min_price}&offer_type=flat&region={city_id}&room0={room0}&type=4".format, max_price=max_price, 
+                       min_price=min_price, 
+                       city_id=city_dict[city], 
+                       deal_type=deal_type, 
+                       room0=1)
+    else:
+        url_base = partial("https://www.cian.ru/cat.php?currency=2&p={page}&deal_type={deal_type}&engine_version=2&sort=creation_date_desc&maxprice={max_price}&minprice={min_price}&offer_type=flat&region={city_id}&room1={room1}&room2={room2}&type=4".format, max_price=max_price, 
+                        min_price=min_price, 
+                        city_id=city_dict[city], 
+                        deal_type=deal_type, 
+                        room1=room1, 
+                        room2=room2)
+    flag = False
+    while True:
+       # import pdb; pdb.set_trace()
+        try:
+            res = session.get(url_base(page=page), headers={'Accept-Language': 'en'})
+        except:
+            continue
+        list_soup = bs4.BeautifulSoup(res.text, "html.parser")
+        offers = list_soup.select("article[data-name='CardComponent']")
+        if (res.url.find("p=1&") != -1) & (page != 1):
+         
+            flag = True
+         
+            page = page - 1
+            res = session.get(url_base(page=page))     
+        list_soup = bs4.BeautifulSoup(res.text, "html.parser")
+        offers = list_soup.select("article[data-name='CardComponent']")
+        if(offers == []):
+            time.sleep(3)
+            print(0)
+            get_urls(i, min_price=i*5000, room1=room1, room2=room2, max_price=((i+1)*5000)-1, city=city, room3=room3, deal_type="rent",no_room=no_room)
+            return
+        for offer in offers:
+                common_data = dict()
+                if deal_type == "sale":
+                    pricing_type = "price"
+                else:
+                    pricing_type = "price_per_month"
+                try:
+                    price = define_price_data(offer)
+                except:
+                    price = min_price//2 + max_price//2
+                try:
+                    price_compare = price[pricing_type]
+                except:
+                    price_compare = price
+                if(min_price <= price_compare <= max_price):
+                    
+                    try:
+                            #import pdb; pdb.set_trace()
+                            common_data['metro_dist'] = offer.find_all("div", {"class":"_93444fe79c--remoteness--q8IXp"})[0].text
+                            
+                    except IndexError:
+                        if (offer.select('div[data-name="SpecialGeo"]') == []):
+                             common_data['metro_dist'] = '6666'
+                        #import pdb; pdb.set_trace()
+                    common_data['title'] = offer.select('div[data-name="GeneralInfoSectionRowComponent"]')[0].select("span")[1].text
+                    common_data['description'] = offer.select('div[data-name="Description"]')[0].select("p")[0].text
+                    common_data['url'] = offer.select("div[data-name='LinkArea']")[0].select("a")[0].get('href')
+                    common_data['time'] = datetime.today().strftime('%Y-%m-%d %H-%M-%S')
+                    location_data = define_location_data(offer, False)
+                    price_data = price
+                    author_data = define_author(offer)
+                    
+                    specification_data = define_specification_data(offer)
+                   
+                    try:
+                        image_data = get_image_data(offer)
+                    except:
+                        image_data = {'image0':" "}
+                    offer_list = offer_list + [union_dicts(author_data, specification_data, price_data, location_data, common_data, image_data)]
+                    url_list.append((offer.select("div[data-name='LinkArea']")[0].select("a")[0].get('href')))
+        old = load_old()
+        if str(i) in old:
+            pass
+        else:
+            old[str(i)] = []
+
+       # 
+        break_flag = False
+        print(datetime.now())
+        if len(offer_list) > 0:
+            if any(d['url'] == offer_list[-1]['url'] for d in old[str(i)]):  
+                flag1 = False  
+                while True:
+                    if(break_flag):
+                        break
+                    #import pdb; pdb.set_trace()
+                    for j in range(-1, -len(offer_list)-1, -1):
+                        if(-len(offer_list) == j):  
+                            break_flag = True
+                        if any(d['url'] == offer_list[j]['url'] for d in old[str(i)]):
+                                offer_list.pop(j)
+                                print(len(offer_list))
+                                break
+                        ##else:
+                         #   break_flag = True
+                        
+        print(datetime.now())
+        page += 1
+        url_list = list(dict.fromkeys(url_list))
+        print(len(url_list))
+        print(res.url)    
+        if(flag or page == 4 or break_flag): #
+            print(break_flag)
+            print(page)
+            offer_list = list({v['url']:v for v in offer_list}.values())
+
+            global offer_list_global
+            offer_list_global[str(i)] = offer_list
+            break
+
+
+def parse( i_min, i_max, room1=1, room2=0, no_room=False):
+    print("room2 = "+str(room2))
+    first_time = True
+                        
+    global offer_list_global
+    offer_list_global = {}
+    #offer_list = []        
+    
+    for i in range(i_min, i_max):
+        get_urls(i, min_price=i*5000, max_price=((i+1)*5000)-1, city="Москва", deal_type="rent",room2=room2,room1=room1,no_room=no_room)
+    
+        location_ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname("goo.py")))
+        try:
+            for root, dirs, files in os.walk(location_):
+                for file in files:
+                    if re.match(r'&.*\.json$', file):
+                        #os.rename(file, str(file)[1:])
+                        filename = file
+                        with open(str(file), "r", encoding='utf-8') as old:
+                            old_json = json.load(old)
+                            if str(i) in old_json:
+                                pass
+                                #disappeared, appeared  = return_non_repetitive_records(old_json[str(i)], offer_list_global[i])
+                            else:
+                                old_json[str(i)] = []
+                                disappeared = []
+                                appeared = []
+                            disappeared = []
+                            #import pdb; pdb.set_trace() 
+                            flagger = False  
+                            old_json[str(i)] = offer_list_updated(offer_list=old_json[str(i)], disappeared=disappeared, appeared=offer_list_global[str(i)]) 
+                            print(offer_list_global[str(i)])
+                            from telegrambot import save_cache 
+                            if(not flagger and len(offer_list_global[str(i)]) > 0):       
+                               # pass
+                                save_cache(offer_list_global[str(i)][:-len(offer_list_global[str(i)])+50])
+                            while True:
+                                try:
+                                    
+                                    with open(filename, "w", encoding='utf-8') as new_file:       
+                                        json.dump(old_json, new_file, ensure_ascii=False, indent = 6)
+                                    break
+                                except:
+                                    pass
+                            
+                            
+                            #time.sleep(30)
+                            raise Sent
+        except Sent:
+            pass
+        
+while True:
+    time.sleep(20)
+   # try:
+    then = datetime.now()
+    parse(2,10,room1=1,room2=1, no_room=True)    
+    parse(5,25,room1=1,room2=1)
+    print(datetime.now() - then )
+   ## except:
+   #     print("error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
---
+++ telegrambot.py
@@ -0,0 +1,344 @@
+
+API_TOKEN = '7006315291:AAE_Nb6L-pNyVi5tFylMycjZnkAYrkkzyYs'  # Замените на ваш токен
+JSON_FILE_PATH = '&rent&30000&40000&1 room&Москва&2024-09-30 22-40-46.json'
+import telebot
+import json
+import time
+import os
+import re
+import datetime
+from datetime import timedelta
+from datetime import datetime
+from copy import deepcopy
+from telebot import types
+
+#API_TOKEN = 'YOUR_API_TOKEN'  # Замените на ваш токен
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Путь к локальному JSON-файлу
+#JSON_FILE_PATH = 'ads.json'  # Укажите путь к вашему локальному JSON-файлу
+last_ads = set()  # Используем множество для хранения уникальных URL объявлений
+PARAMS_FILE = "params.json"
+CACHE_FILE = "cache.json"
+ACTION_FILE = "action"
+ACTION_FILE = "{ACTION_FILE}{date}.json".format(ACTION_FILE=ACTION_FILE, date=datetime.now().strftime('%Y-%m-%d %H-%M'))
+def load_action():
+    try:
+        if os.path.exists(ACTION_FILE):
+            with open(ACTION_FILE, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        else:
+            return {}
+    except:
+        return {}
+    
+
+def save_action(chat_id, action_name):
+    #import pdb; pdb.set_trace()
+    action = load_action()
+    if str(chat_id) not in action.keys():
+        action[str(chat_id)] = {}
+    action[str(chat_id)][datetime.now().strftime('%Y-%m-%d %H-%M-%S')] = action_name
+    
+    with open(ACTION_FILE, 'w', encoding='utf-8') as file:
+        json.dump(action, file, ensure_ascii=False)
+    
+def load_cache():
+    with open(CACHE_FILE, "r", encoding='utf-8') as file:
+       a = json.load(file)
+    return a
+
+def save_cache(appeared):
+    
+    
+    print(len(appeared))
+    all_params = load_parameters()
+    sent_list = {}
+    cache = load_cache()
+    for i in all_params.keys():
+        chat_id = all_params.get(i)['chat_id']
+        new_filtered_ads = filter_ads(appeared, all_params.get(i))
+        sent_list[chat_id] = str(len(new_filtered_ads))
+        print(str(len(new_filtered_ads)) + "длина появившихся")
+        if str(chat_id) not in cache.keys():
+            cache[str(chat_id)] = {}
+        print(i)
+        try:
+            if isinstance(cache[str(chat_id)]['last 20'], list):
+                cache[str(chat_id)]['last 20'] = {}
+        except:
+            pass
+        if 'last 20' in cache[str(chat_id)].keys():
+            pass
+        else:
+            cache[str(chat_id)]['last 20'] = {}
+        if len(cache[str(chat_id)]['last 20'].keys()) > 20:
+            for i in list(cache[str(chat_id)]['last 20'].keys())[:-20]:
+                cache[str(chat_id)]['last 20'].pop(i)
+        if(len(new_filtered_ads)) > 0:
+             
+           # import pdb; pdb.set_trace()            
+            
+            try:
+              #  import pdb; pdb.set_trace()
+                for ad in new_filtered_ads:
+                    cache[str(chat_id)]['last 20'][ad['url']] = ad['description']  
+                    
+                    time_ = datetime.strptime(ad['time'], '%Y-%m-%d %H-%M-%S')
+                    button_bar = types.InlineKeyboardButton('Показать описание', callback_data='{}'.format(ad['url']))
+                    keyboard = types.InlineKeyboardMarkup()
+                    keyboard.add(button_bar)
+                    bot.send_message(chat_id, f"Актуально на {time_}\n"
+                                        f"{ad['title']}"
+                                        f"\n"
+                                        f"🚇Метро: {ad['underground']} {ad['metro_dist']}\n"
+                                        f"🧍‍♂️Автор: {ad['author_type']}\n"
+                                        f"💸Цена: {ad['price_per_month']}₽\n"
+                                        f"🏘Район: {ad['district']}\n"
+                                        f"🛏Количество комнат: {ad.get('rooms_count', 'не указано')}\n"
+                                        f"🔗Источник: {ad['url']}\n"
+                                        f"Изображение: {ad['image0']}", reply_markup=keyboard)
+                bot.send_message(chat_id, text='Появилось {} новых объявления по вашему запросу, чтобы поменять параметры воспользуйтесь командой /start\n'    
+                                 "@KvartiraDar - канал про обновления".format(str(len(new_filtered_ads))))
+            except:
+                print("blocked")
+                pass
+            save_action("sent", sent_list)
+        while True:
+            try:
+                with open(CACHE_FILE, "w+", encoding='utf-8') as file:
+                    json.dump(cache, file, ensure_ascii=False)
+                break
+            except:
+                pass
+def load_parameters():
+    if os.path.exists(PARAMS_FILE):
+        with open(PARAMS_FILE, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    return {}
+
+# Функция для сохранения параметров в файл
+def save_parameters(params):
+    with open(PARAMS_FILE, 'w', encoding='utf-8') as file:
+        json.dump(params, file, ensure_ascii=False)
+
+def get_chat_parameters(chat_id):
+    all_params = load_parameters()
+    return all_params.get(str(chat_id), None)
+# Функция для загрузки объявлений из локального JSON
+def load_ads():
+    with open(JSON_FILE_PATH, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+# Функция для фильтрации объявлений по цене и количеству комнат
+def filter_ads(ads, criteria):
+    filtered = []
+
+    for ad in ads:       
+        for room_criteria in criteria['rooms']:
+            try:
+                print(criteria['metro_dist'])
+            except KeyError:
+                criteria['metro_dist'] = 1000
+            try:
+                print(criteria['author_type'])
+            except KeyError:
+                criteria['author_type'] = "Любой"
+            metro_dist = int(ad['metro_dist'].split(" ")[0])
+            try:
+                if criteria['author_type'] == "Владелец" and ad['author_type'] == "Владелец":
+                    try:
+                        if (metro_dist < criteria['metro_dist'] and room_criteria['min_price'] <= ad['price_per_month'] <= room_criteria['max_price'] and
+                            ad['rooms_count'] == room_criteria['rooms'] and list(filter(re.compile ( criteria['undergrounds'] ).match, ad['underground']))):
+                            filtered.append(ad)
+                    except:    
+                        if (metro_dist < criteria['metro_dist'] and room_criteria['min_price'] <= ad['price_per_month'] <= room_criteria['max_price'] and
+                            ad['rooms_count'] == room_criteria['rooms'] and
+                            (list(filter(re.compile(ad['underground']).match, criteria['undergrounds'])))):
+                        
+                                filtered.append(ad)
+                elif criteria['author_type'] != "Владелец":
+                    try:
+                        if (metro_dist < criteria['metro_dist'] and room_criteria['min_price'] <= ad['price_per_month'] <= room_criteria['max_price'] and
+                            ad['rooms_count'] == room_criteria['rooms'] and list(filter(re.compile ( criteria['undergrounds'] ).match, ad['underground']))):
+                            filtered.append(ad)
+                    except:    
+                        if (metro_dist < criteria['metro_dist'] and room_criteria['min_price'] <= ad['price_per_month'] <= room_criteria['max_price'] and
+                            ad['rooms_count'] == room_criteria['rooms'] and
+                            (list(filter(re.compile(ad['underground']).match, criteria['undergrounds'])))):
+                        
+                                filtered.append(ad)
+                elif criteria['author_type'] == "Владелец" and ad['author_type'] != "Владелец":
+                    break
+            except KeyError:
+                pass        
+    return filtered
+
+
+
+def format_time_passed(item_time):
+    now = datetime.now()
+    time_diff = now - item_time
+
+    if time_diff < timedelta(minutes=1):
+        return "только что"
+    elif time_diff < timedelta(hours=1):
+        minutes = time_diff.seconds // 60
+        return f"{minutes} мин. назад"
+    elif time_diff < timedelta(days=1):
+        hours = time_diff.seconds // 3600
+        return f"{hours} ч. назад"
+    else:
+        days = time_diff.days
+        return f"{days} д. назад"
+# Команда /start
+def test_message(txt):
+    for chat_id in load_parameters().keys():
+        try:
+            bot.send_message(int(chat_id),txt)
+        except:
+            pass
+
+def main():
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_query(call):
+    # import pdb; pdb.set_trace()
+        
+        if "cian.ru" in call.data:
+            try:
+                save_action(call.message.chat.id, 'desc')
+                print(call.message.chat.id)
+                #import pdb; pdb.set_trace()
+                a = load_cache()
+                msg = bot.send_message(call.message.chat.id, f"📔📔 Описание\n {a[str(call.message.chat.id)]['last 20'][call.data]}", reply_to_message_id=call.message.id)
+                bot.pin_chat_message(chat_id=msg.chat.id, message_id=msg.message_id)
+            except:
+                pass
+            
+        # button_foo = types.InlineKeyboardButton('Показать новые', callback_data='new')
+
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        save_action( message.chat.id, 'start')
+        bot.send_message(message.chat.id, "Привет! Я бот для уведомления о новых объявлениях об аренде недвижимости. "
+                                        "Напишите количество комнат интересующей вас квартиры (или несколько через запятую, \n"
+                                        "например: 0,1,2)\n"
+                                        "0 - команты, 1 - однокомнатные и т.д.")
+
+    # Обработка ввода количества комнат
+    @bot.message_handler(func=lambda message: True)
+    def get_rooms(message):
+        try:
+            rooms_input = message.text.split(',')
+            rooms = []
+            
+            for room in rooms_input:
+                room = room.strip()
+                rooms.append({'rooms': int(room), 'min_price': None, 'max_price': None})
+            
+            # Сохраняем количество комнат в контексте пользователя
+            bot.send_message(message.chat.id, "Напишите минимальную арендную плату для {}-комантных квартир. (Например: 30000)".format(rooms[0]['rooms']))
+                                                
+            rooms_copy = []
+            bot.register_next_step_handler(message, lambda msg: get_min_price(msg, rooms, rooms_copy))
+        except ValueError:
+            bot.send_message(message.chat.id, "Пожалуйста, введите корректные числа.")
+
+    def get_min_price(message, rooms, rooms_copy):
+        try:
+            min_price = int(message.text)
+            
+            rooms[0]['min_price'] = min_price
+            bot.send_message(message.chat.id, "Теперь напишите максимальную арендную плату.")
+            bot.register_next_step_handler(message, lambda msg: get_max_price(msg, rooms, rooms_copy))
+        except ValueError:
+            bot.send_message(message.chat.id, "Пожалуйста, введите корректное число.")
+
+    def get_max_price(message, rooms, rooms_copy):
+        save_action(message.chat.id, 'max_price')
+        try:
+            max_price = int(message.text)
+            if max_price < 500:
+                max_price = max_price*1000 
+            rooms[0]['max_price'] = max_price
+            rooms_copy.append(rooms[0])
+            if len(rooms) > 1:
+                # Переход к следующему количеству комнат
+                
+                rooms.pop(0)  # Убираем первый элемент из списка
+                bot.send_message(message.chat.id, "Теперь напишите минимальную арендную плату для {}-комнатных квартир.".format(rooms[0]['rooms']))
+                bot.register_next_step_handler(message, lambda msg: get_min_price(msg, rooms, rooms_copy))
+            else:
+                # Все варианты обработаны
+                bot.send_message(message.chat.id, "Теперь укажите станцию метро с большой буквы (или несколько через запятую, также с большой буквы)."
+                                "Или напишите 'пропустить'")
+                bot.register_next_step_handler(message, lambda msg: get_undergrounds(msg, rooms_copy))
+        except ValueError:
+            bot.send_message(message.chat.id, "Пожалуйста, введите корректное число.")
+
+
+    def get_undergrounds(message, rooms_copy):
+        
+        undergrounds_input = message.text.split(',')
+        undergrounds = [station.strip() for station in undergrounds_input]
+        if (message.text.lower() == "пропустить"):
+            undergrounds=".*"
+        # Сохраняем все введенные данные
+    
+        
+        # Теперь можно использовать criteria для фильтрации объявлений
+        # Например: filtered_ads = filter_ads(all_ads, criteria)
+        
+        bot.send_message(message.chat.id, "Теперь напишите 'да', если хотите получать предложения только от собственников\n"
+                                "А если не хотите, то напишите 'пропустить'")
+        bot.register_next_step_handler(message, lambda msg: get_author_type(msg, rooms_copy, undergrounds))
+        
+    def get_author_type(message, rooms_copy, undergrounds):
+        
+        if message.text.lower() == "да":
+            author_type = "Владелец"
+        else:
+            author_type = "Любой"
+        bot.send_message(message.chat.id, "Теперь укажите максимальное количество минут до метро (например, 10)\n"
+                                "А если не хотите, то напишите 'пропустить'")
+        bot.register_next_step_handler(message, lambda msg: get_metro_dist(msg, rooms_copy, undergrounds, author_type))
+        
+        
+        
+        
+    
+
+    def get_metro_dist(message, rooms_copy, undergrounds, author_type):
+        try:
+            metro_dist = int(message.text)
+        except:
+            metro_dist = 1000
+        all_params = load_parameters()
+        
+            # Сохранение параметров для текущего чата
+        all_params[str(message.chat.id)] = {
+            'rooms': rooms_copy,
+            'undergrounds': undergrounds,
+            'chat_id': message.chat.id,
+            'author_type' : author_type,
+            'metro_dist': metro_dist
+        }
+        save_parameters(all_params)  
+        
+        
+        bot.send_message(message.chat.id, "Ваши параметры сохранены. Я буду уведомлять вас о новых объявлениях.")
+    bot.polling(none_stop=True)
+                        
+    #import threading
+    #threading.Thread(target=watch_json_file, daemon=True).start()
+
+
+    # Запуск бота
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except Exception as e:
+            print(e)
+        
